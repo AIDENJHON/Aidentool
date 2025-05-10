bbc5efc5f842cde3b0924798f38da34f13c1a104
AIDENJHON <09302346197isaiah@gmail.com>
2025-05-10 10:58:01 +0800

    Update Jhonaidenmain.py

--- Jhonaidenmain.py
+++ Jhonaidenmain.py
@@ -226,7 +226,7 @@ def load_player_data(cpm):
         if 'floats' in data and 'localID' in data and 'money' in data and 'coin' in data:
         
             
-            print(Colorate.Horizontal(Colors.rainbow, Center.XCenter(f'Name: {(data.get("Name") if "Name" in data else "UNDEFINED")} 
+           <> print(Colorate.Horizontal(Colors.rainbow, Center.XCenter(f'Name: {(data.get("Name") if "Name" in data else "UNDEFINED")} 
         <> LocalID: {data.get("localID")} 
         <> Money: {data.get("money")} 
             <> Coins: {data.get("coin")}')))
