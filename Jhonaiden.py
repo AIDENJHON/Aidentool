994f72482c4cd38bc84a772dbdbf8870faa1f1f7
AIDENJHON <09302346197isaiah@gmail.com>
2025-05-27 12:53:17 +0800

    Update Jhonaiden.py

--- Jhonaiden.py
+++ Jhonaiden.py
@@ -7,7 +7,7 @@ import json
 
 BASE_URL: str = "https://admincpm.io/Aidenfile/api"
 
-class Pakundo:
+class JaidenMain:
     def __init__(self, access_key) -> None:
         self.auth_token = None
         self.access_key = access_key
